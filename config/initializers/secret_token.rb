9e1aa2ad367a67abeece9bd6feb119ed43cedd54
Wilfried Deluche <wilfried.deluche@gmail.com>
2012-11-21 14:40:46 +0100

    First commit with Rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Findyourgig::Application.config.secret_token = '1f2f198e67e7878cc7511509705e7c40a214935bf437b15bd33b13bf899ac6c52efbbdda15a779b4294d12d57c6b7bdf1bb97bdfb2a1c0d5dfc0a66ce8dea2b1'
