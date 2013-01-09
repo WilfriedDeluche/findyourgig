90e7f88b38c03b61042b058f8c3ae1c10489ffd4
Wilfried Deluche <wilfried.deluche@gmail.com>
2013-01-09 12:48:03 +0100

    Default Users in seed added
    [#39943395]

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
-#   Mayor.create(:name => 'Emanuel', :city => cities.first)
+User.destroy_all
+
+# Default USERS
+user_1 = User.create! email: "admin@findyourgig.com", password: "testtest", password_confirmation: "testtest", first_name: "John", last_name: "Bates"
+puts "User created : #{user_1.first_name} #{user_1.last_name}"
+
+user_2 = User.create! email: "admin2@findyourgig.com", password: "testtest", password_confirmation: "testtest", first_name: "Katy", last_name: "Dockey"
+puts "User created : #{user_2.first_name} #{user_2.last_name}"
