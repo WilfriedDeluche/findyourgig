3ec01eb32d164fb644d7caed43ebf8f538f0d219
Nicolas Bernadine <nicolas.bernadine@gmail.com>
2013-02-03 22:31:31 +0100

    Added some validation, modified the index, the form and localisation

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,3 +1,5 @@
 class Venue < ActiveRecord::Base
-  attr_accessible :name, :address_1, :address_2, :postal_code, :city, :country, :"telephone", :email_address, :website
+  validates_presence_of :name, :address_1, :postal_code, :city, :country
+  validates_uniqueness_of :address_1
+  attr_accessible :name, :address_1, :address_2, :postal_code, :city, :country, :telephone, :email_address, :website
 end
