9477a68d448b6492eb69d8109cdab68beddd534d
Nicolas Bernadine <nicolas.bernadine@gmail.com>
2013-02-09 19:39:29 +0100

    Modifies the display of the distance to nearby venues (including I18n)

--- app/helpers/venues_helper.rb
+++ app/helpers/venues_helper.rb
@@ -1,10 +1,21 @@
 module VenuesHelper
 
-	def distance_display(location)
-		if location.country == "UK"
-			"#{location.distance.to_f.round(2)} miles"
-		else
-			"#{(location.distance.to_f * 1.6).round(2)} km"
-		end
-	end
+  def distance_display(location)
+    #if location.country == "UK"
+    if I18n.locale == :en
+      imperial_distance = location.distance.to_f
+      if (imperial_distance) >= 0.75
+        "#{imperial_distance.round(2)} miles"
+      else
+        "#{(imperial_distance * 1760).round} yards"
+      end
+    else
+      metric_distance = (location.distance.to_f * 1.6)
+      if metric_distance >= 1
+        "#{(metric_distance).round(2)} km"
+      else
+        "#{(metric_distance * 1000).round} m"
+      end
+    end
+  end
 end
