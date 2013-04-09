61b0d809f606b3ef0e3facc59fbb04382e76076c
Wilfried Deluche <wilfried.deluche@gmail.com>
2013-04-09 18:01:59 +0200

    Adds routing tests
    [#39944449]

--- spec/factories/venue_images.rb
+++ spec/factories/venue_images.rb
@@ -1,8 +1,9 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
-  factory :venue_image do
-    file "MyString"
-    main false
+  factory :venue_image do |image|
+    image.file "MyString"
+    image.is_main false
+    image.venue :venue
   end
 end
