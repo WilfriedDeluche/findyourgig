f2183a2877978a62a1cd8bec8a8cd016b8552389
Nicolas Bernadine <nicolas.bernadine@gmail.com>
2013-04-24 10:19:59 +0200

    Corrects an anomaly in the date format and adds validation on gig hours

--- app/models/gig.rb
+++ app/models/gig.rb
@@ -7,4 +7,17 @@ class Gig < ActiveRecord::Base
   attr_accessible :concert_end_time, :concert_start_time, :description, :doors_time, :name, :soundcheck_time, :venue_id
 
   validates_presence_of :concert_start_time, :description, :doors_time, :name, :venue_id
+  validate :concert_start_time_okay, :concert_end_time_okay, :doors_time_okay
+
+	def doors_time_okay
+  	errors.add :country, I18n.t('incorrect_doors_time') if !soundcheck_time.blank? && doors_time<soundcheck_time
+  end
+
+  def concert_start_time_okay
+  	errors.add :country, I18n.t('incorrect_concert_start_time') if !doors_time.blank? && concert_start_time<doors_time
+  end
+
+  def concert_end_time_okay
+  	errors.add :country, I18n.t('incorrect_concert_end_time') if !concert_end_time.blank? && concert_end_time<concert_start_time
+  end
 end
